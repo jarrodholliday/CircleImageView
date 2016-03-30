6feff9359a91dafa5bc166a5116f89084a2036ea
Alex <zhukovich.a@gmail.com>
2016-03-30 22:18:22 +0200

    Example was simplified

--- circleimageviewexample/src/main/java/com/alexzh/circleimageviewexample/MainActivity.java
+++ circleimageviewexample/src/main/java/com/alexzh/circleimageviewexample/MainActivity.java
@@ -1,63 +1,47 @@
 package com.alexzh.circleimageviewexample;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import com.alexzh.circleimageview.CircleImageView;
+import com.alexzh.circleimageview.ItemSelectedListener;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,
-        NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements ItemSelectedListener {
 
-    private DrawerLayout mDrawerLayout;
+    private TextView mSelectedTextView;
+    private TextView mUnSelectedTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        setUpToolbar();
-        ((NavigationView)findViewById(R.id.navigation)).setNavigationItemSelectedListener(this);
-
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new ProfileFragment())
-                .commit();
-    }
-
-    private void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
-            setSupportActionBar(toolbar);
-            toolbar.setNavigationOnClickListener(this);
+        setSupportActionBar(toolbar);
+
+        mSelectedTextView = (TextView) findViewById(R.id.selectedTextView);
+        mUnSelectedTextView = (TextView) findViewById(R.id.unselectedTextView);
+
+        CircleImageView circleImageView = (CircleImageView) findViewById(R.id.imageView);
+        if (circleImageView != null) {
+            circleImageView.setOnItemSelectedClickListener(this);
         }
     }
 
     @Override
-    public void onClick(View v) {
-        mDrawerLayout.openDrawer(GravityCompat.START);
+    public void onSelected(View view) {
+        mSelectedTextView.setVisibility(View.VISIBLE);
+        mUnSelectedTextView.setVisibility(View.GONE);
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem menuItem) {
-        menuItem.setChecked(true);
-        switch (menuItem.getItemId()) {
-            case R.id.item_profile:
-                mDrawerLayout.closeDrawers();
-                Snackbar.make(findViewById(R.id.container), R.string.action_profile, Snackbar.LENGTH_SHORT).show();
-                return true;
-            case R.id.item_settings:
-                mDrawerLayout.closeDrawers();
-                Snackbar.make(findViewById(R.id.container), R.string.action_settings, Snackbar.LENGTH_SHORT).show();
-                return true;
-        }
-        return false;
+    public void onUnselected(View view) {
+        mSelectedTextView.setVisibility(View.GONE);
+        mUnSelectedTextView.setVisibility(View.VISIBLE);
     }
 }
 
